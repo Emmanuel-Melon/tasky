e5f7d48fec84df6d7aa86c11c8e29b5faa06be6a
Eman <emmanuelgatwech@gmail.com>
2024-03-29 12:07:58 +0200

    feat: Added a route to handle GET requests for fetching a list of tasks from a dummy array stored on the server.

--- apps/server/src/tasks/router.ts
+++ apps/server/src/tasks/router.ts
@@ -1,9 +1,12 @@
 import { NextFunction, Response, Router, Request } from 'express';
+import { listUserTasks } from './operations/list';
+
 const tasksRouter = Router();
 
-tasksRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+tasksRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  const tasks = await listUserTasks("1");
   res.status(200).json({
-    tasks: []
+    tasks
   });
 });
 
