c377c445dff070d428ace1d14a3669781b79e665
Eman <emmanuelgatwech@gmail.com>
2024-03-29 11:45:28 +0200

    feat: add task overview controls

--- apps/web/src/Tasks/ListTasks.svelte
+++ apps/web/src/Tasks/ListTasks.svelte
@@ -1,31 +1,54 @@
 <script lang="ts">
 	import TaskOverview from './TaskOverview.svelte';
-    import ListControls from './ListControls.svelte';
+	import ListControls from './ListControls.svelte';
+	import autoAnimate from '@formkit/auto-animate';
 	const tasks = [
 		{
 			id: 1,
 			title: '[TASK-2]: List Tasks',
 			description: 'Build a tasks list',
-			deadline: 'Friday 29 March'
+			deadline: '2024-4-11T10:37:01.188224+00:00',
+			users: [
+				{
+					id: 1,
+					name: 'Emmanuel Gatwech',
+					avatar: 'https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg'
+				}
+			]
 		},
 		{
 			id: 2,
 			title: '[TASK-3]: View Task Details',
 			description: 'Build a tasks list',
-			deadline: 'Friday 29 March'
+			deadline: '2024-4-11T10:37:01.188224+00:00',
+			users: [
+				{
+					id: 1,
+					name: 'Emmanuel Gatwech',
+					avatar: 'https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg'
+				},
+				{
+					id: 2,
+					name: 'Emmanuel Gatwech',
+					avatar: 'https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg'
+				}
+			]
 		},
 		{
 			id: 3,
 			title: '[TASK-4]: Edit Task',
 			description: 'Build a tasks list',
-			deadline: 'Friday 29 March'
+			deadline: '2024-4-11T10:37:01.188224+00:00',
+			users: []
 		}
 	];
 </script>
 
 <div class="space-y-4">
 	<ListControls />
-	{#each tasks as task}
-		<TaskOverview {task} />
-	{/each}
+	<div use:autoAnimate class="space-y-4">
+		{#each tasks as task}
+			<TaskOverview {task} />
+		{/each}
+	</div>
 </div>
---
+++ packages/ui/components/Avatar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let src = "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg";
+    export let alt = "Failed to load image";
+</script>
+
+<div class="avatar">
+  <div class="w-10 rounded-full">
+    <img src={src} alt={alt}/>
+  </div>
+</div>
