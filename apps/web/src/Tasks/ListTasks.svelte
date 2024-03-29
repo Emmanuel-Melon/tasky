58007cc07250f009533e559b76b369bb5e822706
Eman <emmanuelgatwech@gmail.com>
2024-03-29 10:43:06 +0200

    feat: list tasks component

---
+++ apps/web/src/Tasks/ListTasks.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+	import TaskOverview from './TaskOverview.svelte';
+	const tasks = [
+		{
+			id: 1,
+			title: '[TASK-2]: List Tasks',
+			description: 'Build a tasks list',
+			deadline: 'Friday 29 March'
+		},
+		{
+			id: 2,
+			title: '[TASK-3]: View Task Details',
+			description: 'Build a tasks list',
+			deadline: 'Friday 29 March'
+		},
+		{
+			id: 3,
+			title: '[TASK-4]: Edit Task',
+			description: 'Build a tasks list',
+			deadline: 'Friday 29 March'
+		}
+	];
+</script>
+
+<div class="space-y-4">
+    <div class="flex items-center justify-between">
+        <div class="">
+            <h3 class="text-xl">My Tasks</h3>
+        </div>
+        <div class="">
+            <button class="btn btn-primary">Add Task <Icon icon="heroicons:plus-circle" /></button>
+        </div>
+    </div>
+	{#each tasks as task}
+		<TaskOverview {task} />
+	{/each}
+</div>
---
+++ apps/web/src/Tasks/TaskOverview.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let task: any;
+</script>
+
+
+<div class="card card-bordered card-compact">
+    <div class="card-body">
+        <h3 class="card-title">{task.title}</h3>
+        <p>{task.description}</p>
+    </div>
+</div>
