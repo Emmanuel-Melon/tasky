5f2e67be3115d695239a46103eadce861ba6b332
Eman <emmanuelgatwech@gmail.com>
2024-04-01 12:50:27 +0200

    feat: add filter and sorting queries

--- apps/server/src/tasks/operations/create.ts
+++ apps/server/src/tasks/operations/create.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { prisma } from "../../lib/prisma";
+import { Task } from '@prisma/client';
 
 export const taskSchema = z.object({
     title: z.string(),
@@ -10,7 +11,7 @@ export const taskSchema = z.object({
 
 export type TaskAttributes = z.infer<typeof taskSchema>;
 
-export const createNewTask = async (taskAttributes: any): Promise<any> => {
+export const createNewTask = async (taskAttributes: any): Promise<Task> => {
     return await prisma.task.create({
         data: {
             ...taskAttributes
