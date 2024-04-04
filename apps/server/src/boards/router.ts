9270acff66bcfcc4dd78f5c791fe75a5f86e8a44
Eman <emmanuelgatwech@gmail.com>
2024-04-04 11:26:06 +0200

    feat: fetch lists and boards from the database

--- apps/server/src/boards/router.ts
+++ apps/server/src/boards/router.ts
@@ -1,20 +1,31 @@
 import { NextFunction, Response, Router, Request } from 'express';
 import { listUserBoards } from './operations/list';
-import { createNewBoard } from './operations/create';
+import { createNewBoard, createNewList } from './operations/create';
+import { findBoardById, findListById } from './operations/findById';
 
 const tasksRouter = Router();
 
-
 tasksRouter.post('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const board = await createNewBoard(req.body);
-      res.status(201).json({
-        board
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
+  try {
+    const board = await createNewBoard(req.body);
+    res.status(201).json({
+      board
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+tasksRouter.post('/lists', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const list = await createNewList(req.body);
+    res.status(201).json({
+      list
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 tasksRouter.get(
   '/:boardId/lists',
@@ -36,5 +47,33 @@ tasksRouter.get(
   }
 );
 
+tasksRouter.get(
+    '/:boardId',
+    async (req: Request<any, any, any>, res: Response, next: NextFunction) => {
+      try {
+        const list = await findBoardById(req.params.boardId);
+        res.status(200).json({
+          list
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+
+
+tasksRouter.get(
+    '/lists/:listId',
+    async (req: Request<any, any, any>, res: Response, next: NextFunction) => {
+      try {
+        const list = await findListById(req.params.listId);
+        res.status(200).json({
+          list
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
 
 export default tasksRouter;
