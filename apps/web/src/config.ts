b760c5a8ee4f5a45abd37fdd8cd3964162d7b56e
Eman <emmanuelgatwech@gmail.com>
2024-04-04 20:23:18 +0200

    feat: docker builds for tasky web and server

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -1,4 +1,2 @@
 import { PUBLIC_BACKEND_URL } from '$env/static/public';
-
-console.log("PUBLIC_BACKEND_URL", PUBLIC_BACKEND_URL);
 export const backendUrl = PUBLIC_BACKEND_URL ?? 'http://localhost:8000/api';
